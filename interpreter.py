52e4b6300436afa471f07458ac9b374df4aaa369
mrunknown850 <minh0608@outlook.com>
2024-05-02 20:13:27 +0700

    Finished the basic of the tokenizer of both variable and layout. Added some basic classes.

--- interpreter.py
+++ interpreter.py
@@ -1,8 +1,34 @@
 from os import linesep
 from sys import argv
 from collections import deque
+import random
+
+# def randomString(length: int) -> str:
+
+class ConstantInteger:
+    def __init__(self, low: int, high: int) -> None:
+        self.value: int = random.randint(low, high)
+    def __repr__(self) -> int:
+        return self.value
+    
+class RandomInteger:
+    def __init__(self, low: int, high: int) -> None:
+        self.low: int = low
+        self.high: int = high
+    def __repr__(self) -> int:
+        return random.randint(low, high)
+
+FILENAME = ""
+CURRENT_LINE = 0
+
+class InvalidKeyword(Exception):
+    def __init__(self):
+        super().__init__(f'File "{FILENAME}", line {CURRENT_LINE}')
 
 def variable_tokenizer(variable_line: str) -> dict | None:
+    global CURRENT_LINE
+    CURRENT_LINE += 1
+    
     token = {}
     words = variable_line.split()
 
@@ -20,56 +46,183 @@ def variable_tokenizer(variable_line: str) -> dict | None:
                 case "str":
                     token['type'] = 'str'
                 case _:
-                    token['name'] = keyword[1:]
-                    isCompleted = True
+                    # If initializing a variable name
+                    if keyword[0] == "%":
+                        token['name'] = keyword[1:]
+                        isCompleted = True
+                    else:
+                        raise InvalidKeyword
             continue
         if token['type'] == 'int':
-            token['range'] = tuple( [int(words[num]), int(words[num+1][:-1]) ])
+            try:
+                token['range'] = tuple( sorted( [int(words[num]), int(words[num+1]) ] ))
+            except ValueError:
+                raise InvalidKeyword
             break
         
+
     if 'isConst' not in token.keys():
         token['isConst'] = False
 
     return token
 
 def layout_tokenizer(line_stack: list) -> tuple | None:
+    global CURRENT_LINE
+
+    IS_VERTICAL = False
+    IS_HORIZONTAL = False
+    
     tokens = []
+    currentToken = {}
 
     for index, line in enumerate(line_stack):
-        # Logic for processing the 2 built-in variables
-        if 'vertical' in line or 'horizontal' in line:
-            pass
-        else:
-            # Breaking up the lines
-            words = line.split()
-            # for word in words:
-    
-    
+        CURRENT_LINE += 1
+        # Removing the \n at the end of every line
+        line = line.replace('\n', '')
+
+        # Spliting the line to words
+        words = line.split()
+        if len(words) == 0:
+            continue
+
+        # TRUTH TABLE
+        if not IS_VERTICAL and not IS_HORIZONTAL:
+            # * Both VERTICAL and HORIZONTAL are available
+            match words[0]:
+                case 'vertical':
+                    IS_VERTICAL = True
+
+                    currentToken['type'] = 'v-array'
+                    currentToken['contain'] = []
+
+                    if words[1][0] == "$":
+                        currentToken['length'] = words[1][1:]
+                    else:
+                        try:
+                            currentToken['length'] = int(words[1])
+                        except ValueError:
+                            raise InvalidKeyword
+                case 'horizontal':
+                    IS_HORIZONTAL = True
+
+                    currentToken['type'] = {'h-array'}
+                    currentToken['contain'] = []
+
+                    if words[1][0] == "$":
+                        currentToken['length'] = words[1][1:]
+                    else:
+                        try:
+                            currentToken['length'] = int(words[1])
+                        except ValueError:
+                            raise InvalidKeyword
+                case 'v-end':
+                    raise InvalidKeyword
+                case 'h-end':
+                    raise InvalidKeyword
+                case _:
+                    currentToken['type'] = 'list'
+                    currentToken['contain'] = []
+                    for word in words:
+                        if word[0] == "$":
+                            currentToken['contain'].append({'type': 'var', 'value': word[1:]})
+                        else:
+                            currentToken['contain'].append({'type': 'const', 'value': word})
+        elif IS_VERTICAL and not IS_HORIZONTAL:
+            subToken = {}
+            # * Only the HORIZONTAL is available
+            match words[0]:
+                case 'vertical':
+                    raise InvalidKeyword
+                case 'horizontal':
+                    IS_HORIZONTAL = True
+
+                    subToken['type'] = 'h-array'
+                    subToken['contain'] = []
+
+                    if words[1][0] == "$":
+                        subToken['length'] = words[1][1:]
+                    else:
+                        try:
+                            subToken['length'] = int(words[1])
+                        except ValueError:
+                            raise InvalidKeyword
+                case 'v-end':
+                    IS_VERTICAL = False
+                case 'h-end':
+                    raise InvalidKeyword
+                case _:
+                    subToken['type'] = 'list'
+                    subToken['contain'] = []
+                    for word in words:
+                        if word[0] == "$":
+                            subToken['contain'].append({'type': 'var', 'value': word[1:]})
+                        else:
+                            subToken['contain'].append({'type': 'const', 'value': word})
+            if len(subToken) != 0:
+                currentToken['contain'].append(subToken)
+        elif not IS_VERTICAL and IS_HORIZONTAL:
+            # * RAISE EXCEPTION
+            raise InvalidKeyword
+        elif IS_VERTICAL and IS_HORIZONTAL:
+            subToken = {}
+            # * Only the HORIZONTAL is available
+            match words[0]:
+                case 'vertical':
+                    raise InvalidKeyword
+                case 'horizontal':
+                    raise InvalidKeyword
+                case 'v-end':
+                    raise InvalidKeyword
+                case 'h-end':
+                    IS_HORIZONTAL = False
+                case _:
+                    subToken['type'] = 'list'
+                    subToken['contain'] = []
+                    for word in words:
+                        if word[0] == "$":
+                            subToken['contain'].append({'type': 'var', 'value': word[1:]})
+                        else:
+                            subToken['contain'].append({'type': 'const', 'value': word})
+            if len(subToken) != 0:
+                currentToken['contain'][-1]['contain'].append(subToken)
+        
+        if not IS_HORIZONTAL and not IS_VERTICAL:
+            tokens.append(currentToken)
+            currentToken = {}
     return tuple(tokens)
 
 def tokenizer(lines: deque) -> dict:
+    global CURRENT_LINE
     variableTokens = []
 
-    # Scanning until reaching 
+    # * VARIABLE SECTION
     currentLine: str = lines.popleft()
     currentLine = currentLine.replace('\n', '')
-    while currentLine != "<":
+    while currentLine != "BEGIN":
         returnToken = variable_tokenizer(currentLine)
         if returnToken is not None:
             variableTokens.append(returnToken)
         currentLine = lines.popleft()
         currentLine = currentLine.replace('\n', '')
-    lines.pop()
+    CURRENT_LINE += 1
+
+    # * LAYOUT SECTION
+    currentLine = lines.pop()
+    while currentLine != "END":
+        currentLine = lines.pop()
     layoutTokens = layout_tokenizer(list(lines))
 
-    return tuple(variableTokens, layout_tokenizer)
+    return tuple([variableTokens, layoutTokens])
 
 def main(path: str):
     # Reading the file itself
-    with open(path, 'r') as f:
+    global FILENAME
+    FILENAME = path
+    with open(FILENAME, 'r') as f:
         lines = f.readlines()
     lines = deque(lines)
     tokens = tokenizer(lines)
+    print()
 
 if __name__ == "__main__":
     # Retrieving the given file path
